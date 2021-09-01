139e8e4eb6ced14e97f87d66645553c216406665
jerger1 <thecapslock2c@gmail.com>
2021-09-02 00:04:56 +0200

    ajaxtabletest

--- index.js
+++ index.js
@@ -1,26 +1,9 @@
 // see docs at https://datatables.net/
 $(document).ready(function() {
   $('#example').dataTable( {
-    data: data
+    "ajax": "ajaxtest.json"
   } );
 } );
 
-var data = [
-  [
-      "Tiger Nixon",
-      "System Architect",
-      "Edinburgh",
-      "5421",
-      "2011/04/25",
-      "$3,120"
-  ],
-  [
-      "Garrett Winters",
-      "Director",
-      "Edinburgh",
-      "8422",
-      "2011/07/25",
-      "$5,300"
-  ]
-]
+
 
