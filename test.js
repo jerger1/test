18a9daf0cfe5029c17d620ad133f795a734bf58f
jerger1 <thecapslock2c@gmail.com>
2021-09-04 23:14:25 +0200

    api test

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-const fetchData = async () => {
+/*const fetchData = async () => {
     const res = await fetch("https://www.parsehub.com/api/v2/projects/tF1VrLrwD12p/last_ready_run/data?api_key=tVdieidOdjrx&format=json"); // fetch() returns a promise, so we need to wait for it
   
     const country = await res.json(); // res is now only an HTTP response, so we need to call res.json()
@@ -7,4 +7,4 @@ const fetchData = async () => {
    document.getElementById("test").innerHTML= JSON.stringify(country); 
 };
   
-  fetchData();
+  fetchData();*/
