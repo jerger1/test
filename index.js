57ebd50b3fc81e7b0f1e228f0c072404020a30af
jerger1 <thecapslock2c@gmail.com>
2021-09-04 23:00:06 +0200

    api test

--- index.js
+++ index.js
@@ -2,6 +2,14 @@
 $(document).ready(function() {
   $('#example').dataTable( {
     "ajax": "ajaxtest.json",
+    "columns": [
+      { "data": "name" },
+      { "data": "url" },
+      { "data": "isin" },
+      { "data": "Position" },
+      { "data": "ArtdesInsruments" },
+      
+  ],
     dom: 'Bfrtip',
     buttons: [
         'copy', 'excel', 'pdf'
